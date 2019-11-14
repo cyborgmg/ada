c18d8b45efc4dd26e19b762fdeb33a345a5cf177
Rodrigo <rodrigo.mendes@smartsw.com.br>
2019-11-14 12:43:04 -0300

    add project redis-jwt

---
+++ spring-boot-redis-jwt/src/main/java/np/com/ngopal/spring/boot/redis/domain/SessionUser.java
@@ -0,0 +1,72 @@
+
+package np.com.ngopal.spring.boot.redis.domain;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import lombok.*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author NGM
+ */
+@Data
+public class SessionUser {
+
+    private String username;
+
+    private String password;
+
+    private Date created;
+
+    public String getUsername() {
+		return username;
+	}
+
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+	public Date getCreated() {
+		return created;
+	}
+
+
+
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+
+
+
+	public boolean hasExpired() {
+        if(created == null){
+            return true;
+        }
+        LocalDateTime localDateTime = created.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+        localDateTime = localDateTime.plusHours(1);
+        return  Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant()).before(new Date());
+    }
+
+}
